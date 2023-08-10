cf87d1bf64f79d3228317de829839a2c925b91e8
Rajin <rajinradhakrishnan99@gmail.com>
2023-08-10 13:06:36 +0530

    model folder is added

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require("express");
+const mongoose = require('mongoose')
 
 const app = express();
+
+mongoose.connect()
+  .then((req,res)=>{
+    res.send('connected to db')
+  })
+  .catch(err=> {
+    res.send('connection failed',err)
+  })
 
 app.listen(5000, () => {
   console.log("Server started on port: 5000");
