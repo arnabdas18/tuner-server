0d736a31ce1554e71d96b12df4a3a3292d610491
Rajin <rajinradhakrishnan99@gmail.com>
2023-08-10 22:35:47 +0530

    logIn code changed

--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -1,82 +1,74 @@
 
 const express = require('express')
 const userRoute = express.Router()
-const userSchema = require('../model/userSchema')
+const userModel = require('../model/userSchema')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 
-userRoute.post('/register', (req,res)=>{
+userRoute.post('/register', async (req, res) => {
 
-    const {name,email,phone,profession,password} = req.body
-    
-        const hashedPassword = bcrypt.hash(password,10)
-        const post = new userSchema({
-            name,
-            email,
-            password : hashedPassword,
-            phone,
-            profession
+    const { name, email, phone, profession, password } = req.body
+
+    const hashedPassword = await bcrypt.hash(password, 10)
+    const post = new userModel({
+        name,
+        email,
+        password: hashedPassword,
+        phone,
+        profession
+    })
+
+    post.save()
+        .then((result) => {
+            res.status(201).json({
+                message: 'registered succesfully',
+                data: result
+            })
         })
-
-        post.save()
-            .then((result)=>{
-                res.status(201).json({
-                    message : 'registered succesfully',
-                    data : result
-                })
+        .catch(err => {
+            res.status(500).json({
+                message: 'user already registered',
+                error: err
             })
-            .catch(err => {
-                res.status(500).json({
-                    message : 'user already registered',
-                    error : err
-                })
-            })
+        })
 
 
 })
 
 
-userRoute.post('/login', (req,res)=>{
+userRoute.post('/login', async (req, res) => {
+    try {
+        const { email, password } = req.body;
 
-    const {email,password} = req.body;
-    userSchema.findOne({email})
-        .then(user => {
-            if(user){
-                bcrypt.compare(password,result.password)
-                .then(pwdResponse => {
-                    if(pwdResponse){
-                        const token = jwt.sign({id : user._id, email : user.email }, 'TunerAppGroup13',  {expiresIn : "5d"})
-                        res.status(200).json({
-                            message : 'User Logged in succesfully',
-                            token
-                        })
-                    }
-                    else{
-                        res.status(400).json({
-                            message : 'email or password incorrect'
-                        })
-                    }
-                  
-                })
-                .catch(err => {
-                    res.status(400).json({
-                        message : 'internal server error'
-                    })
-                })
-            }
-            else{
-                res.status(400).json({
-                    message : 'user not registered'
-                })
-            }
-          
-        })
-        .catch(err =>{
-            res.status(500).json({
-                message : 'unknown error',
-                error : err
-            })
-        })
+        const user = await userModel.findOne({ email });
+        if (!user) {
+            return res.status(404).send({
+                success: false,
+                message: "Email is not registered",
+            });
+        }
+
+        const match = await bcrypt.compare(password, user.password);
+        if (!match) {
+            return res.status(200).send({
+                success: false,
+                message: "Invalid Password",
+            });
+        }
+        const token = await jwt.sign({ _id: user._id }, 'TunerAppGroup13', { expiresIn: "7d" });
+        res.status(200).send({
+            message: "login successfully",
+            token,
+        });
+    }
+    catch (error) {
+        console.log(error);
+        res.status(500).send({
+            success: false,
+            message: "Error in login",
+            error,
+        });
+    }
 })
 
 module.exports = userRoute
