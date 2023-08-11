9d2549d2759617f89cc3415ae1853a2c1f1fdd1f
Arnab Das <86297998+arnabdas18@users.noreply.github.com>
2023-08-11 18:56:55 +0530

    (added) video routes completed

---
+++ src/middleware/authMiddleware.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+
+module.exports = async (req, res, next) => {
+  try {
+    const decode = jwt.verify(
+      req.headers.authorization,
+      process.env.JWT_SECRET
+    );
+    req.user = decode;
+    next();
+  } catch (error) {
+    console.log(error);
+  }
+};
