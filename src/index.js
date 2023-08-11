a0d54a3907abdb4b72733cbb312ee4f75a2c9ab1
Arnab Das <86297998+arnabdas18@users.noreply.github.com>
2023-08-11 19:04:04 +0530

    (modified) included userRoutes and videoRoutes

--- src/index.js
+++ src/index.js
@@ -4,10 +4,16 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
+const userRoutes = require("./routes/user.routes");
+const videoRoutes = require("./routes/video.routes");
+
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
+
+app.use("/api/v1/auth", authRoutes);
+app.use("/", videoRouter);
 
 mongoose
   .connect(process.env.MONGO_URI)
