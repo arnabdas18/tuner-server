e104fd3c3a0bad47222868a00fc391e079b44a5b
Rajin <rajinradhakrishnan99@gmail.com>
2023-08-12 18:13:03 +0530

    deleted unwanted comments

--- src/Routes/user.routes.js
+++ src/Routes/user.routes.js
@@ -6,36 +6,36 @@ const jwt = require("jsonwebtoken");
 const userRoute = express.Router();
 
 userRoute.post("/register", async (req, res) => {
-  
-    const { name, email, phone, profession, password } = req.body;
 
-    const hashedPassword = await bcrypt.hash(password, 10);
+  const { name, email, phone, profession, password } = req.body;
 
-    const user = new userModel({
-      name,
-      email,
-      password: hashedPassword,
-      phone,
-      profession,
-    });
+  const hashedPassword = await bcrypt.hash(password, 10);
 
-    // const result = await user.save();
+  const user = new userModel({
+    name,
+    email,
+    password: hashedPassword,
+    phone,
+    profession,
+  });
 
-    user.save()
-        .then((result) => {
-            res.status(201).json({
-              success : true,
-                message: 'Registered succesfully',
-                data: result
-            })
-        })
-        .catch(err => {
-            res.status(500).json({
-              success : false,
-                message: 'User already registered',
-                error: err
-            })
-        })
+
+
+  user.save()
+    .then((result) => {
+      res.status(201).json({
+        success: true,
+        message: 'Registered succesfully',
+        data: result
+      })
+    })
+    .catch(err => {
+      res.status(500).json({
+        success: false,
+        message: 'User already registered',
+        error: err
+      })
+    })
 });
 
 userRoute.post("/login", async (req, res) => {
@@ -44,7 +44,7 @@ userRoute.post("/login", async (req, res) => {
 
     const user = await userModel.findOne({ email });
     if (!user) {
-      return res.status(404).send({
+      return res.status(404).json({
         success: false,
         message: "Email is not registered",
       });
@@ -52,21 +52,26 @@ userRoute.post("/login", async (req, res) => {
 
     const match = await bcrypt.compare(password, user.password);
     if (!match) {
-      return res.status(200).send({
+      return res.status(400).json({
         success: false,
-        message: "Invalid Password",
+        message: "Invalid Email or Password",
       });
     }
     const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
       expiresIn: "7d",
     });
-    res.status(200).send({
+    res.status(200).json({
+      success: true,
       message: "LoggedIn successfully",
-      token,
+      data :{
+        userId: user._id,
+        token,
+      }
+     
     });
   } catch (error) {
-    console.log(error);
-    res.status(500).send({
+    
+    res.status(500).json({
       success: false,
       message: "Error in login",
       error,
